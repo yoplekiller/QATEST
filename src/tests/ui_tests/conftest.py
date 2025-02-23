d59a3c04c1dd4036b20dbd81c31c9153ae5d7995
yoplekiller <jmlim9244@gmail.com>
2025-02-23 23:32:03 +0900

    fix

--- src/tests/ui_tests/conftest.py
+++ src/tests/ui_tests/conftest.py
@@ -13,7 +13,9 @@ def driver():
 
     chrome_options.add_argument("--disable-dev-shm-usage") #메모리 부족 방지
     chrome_options.add_argument("--no-sandbox") #샌드박스 비활성화
-    # chrome_options.add_argument("--headless") #GUI없는 환경에서도 실행 가능
+    chrome_options.add_argument("--headless") #GUI없는 환경에서도 실행 가능
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--remote-debugging-port=9222")
 
 
     service = Service(ChromeDriverManager().install())
