b1a458fbd7012b4bd0bbf8f87ac4ef018a55dda2
yoplekiller <jmlim9244@gmail.com>
2025-02-24 21:06:57 +0900

    fix

--- project/config.py
+++
@@ -1,3 +0,0 @@
-# 기본 주소
-BASE_URL ="https://www.kurly.com/main"
-API_BASE_URL = "http://127.0.0.1:5000"
--- tests/api/conftest.py
+++
@@ -1,8 +0,0 @@
-import os
-
-import pytest
-
-
-@pytest.fixture(scope="session")
-def api_url():
-    return os.getenv("API_BASE_URL", "http://localhost:5000")
