ce93ae7fa41884d2c9f5f364fe6a6e590741e1ee
yoplekiller <jmlim9244@gmail.com>
2025-03-03 02:47:06 +0900

    fix

--- src/tests/ui_tests/test_ui_cart.py
+++ src/tests/ui_tests/test_ui_cart.py
@@ -1,8 +1,10 @@
 import time
+import allure
 from selenium.webdriver.common.by import By
 from utils.utilites import capture_screenshot
 
-
+@allure.feature("UI 테스트")
+@allure.story("장바구니 화면 체크 테스트")
 def test_cart(driver):
     cart_button = driver.find_element(By.XPATH,"//button[@class='css-g25h97 e14oy6dx1']")
     cart_button.click()
