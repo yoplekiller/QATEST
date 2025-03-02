0cab19a67f938259dac460642110ee4827ee386d
yoplekiller <yoplekiller@users.noreply.github.com>
2025-03-02 17:24:56 +0000

    deploy: fb48d48d852bf72eb77849f1e59c9f07a4f41aff

---
+++ src/tests/ui_tests/test_ui_search.py
@@ -0,0 +1,34 @@
+import pytest
+from selenium.webdriver import Keys
+import time
+from utils.excel_reader import read_search_terms_from_excel, file_path
+from selenium.webdriver.common.by import By
+from utils.utilites import capture_screenshot
+
+
+search_cases = read_search_terms_from_excel(file_path)
+
+@pytest.mark.parametrize("tc_id, search_term", search_cases)
+def test_product_search(driver, tc_id, search_term):
+    print(f"🔍 TC {tc_id}: '{search_term}' 검색 테스트 실행 중...")
+
+
+    search_box = driver.find_element(By.XPATH, "//input[@id='gnb_search']")
+    time.sleep(2)
+    for _ in range(10):
+        search_box.send_keys(Keys.BACKSPACE)
+
+    search_box.send_keys(search_term)
+    search_box.send_keys(Keys.RETURN)
+    time.sleep(2)
+
+    capture_screenshot(driver, f"{tc_id}_{search_term}", 'screenshots_Search')
+
+
+
+
+
+
+
+
+
