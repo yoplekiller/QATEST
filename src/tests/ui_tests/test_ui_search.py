bfec69bbf933657801f065dac4935f9ec8cb7cdf
yoplekiller <yoplekiller@users.noreply.github.com>
2025-03-06 12:13:08 +0000

    deploy: b0b51aaf3da86565ccdcee78ecdd25b31b2b8d87

--- src/tests/ui_tests/test_ui_search.py
+++ src/tests/ui_tests/test_ui_search.py
@@ -14,18 +14,25 @@ search_cases = read_search_terms_from_excel(file_path)
 @pytest.mark.parametrize("tc_id, search_term", search_cases)
 def test_product_search(driver, tc_id, search_term):
     print(f"🔍 TC {tc_id}: '{search_term}' 검색 테스트 실행 중...")
+    try:
+        search_box = driver.find_element(By.XPATH, "//input[@id='gnb_search']")
+        time.sleep(2)
+        for _ in range(10):
+            search_box.send_keys(Keys.BACKSPACE)
+
+        search_box.send_keys(search_term)
+        search_box.send_keys(Keys.RETURN)
+        time.sleep(2)
+
+    except Exception as e:
+        capture_screenshot(driver, f"{tc_id}_{search_term}", 'screenshots_Search')
+        pytest.fail(f"❌ 검색 실패: {str(e)}")
 
 
-    search_box = driver.find_element(By.XPATH, "//input[@id='gnb_search']")
-    time.sleep(2)
-    for _ in range(10):
-        search_box.send_keys(Keys.BACKSPACE)
 
-    search_box.send_keys(search_term)
-    search_box.send_keys(Keys.RETURN)
-    time.sleep(2)
 
-    capture_screenshot(driver, f"{tc_id}_{search_term}", 'screenshots_Search')
+
+
 
 
 
