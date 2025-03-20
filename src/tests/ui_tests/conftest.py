bf8cecad7fb8eb493e765174f78ef931b13a7318
yoplekiller <yoplekiller@users.noreply.github.com>
2025-03-20 18:51:53 +0000

    deploy: 688021e0149d4b8ef8d8116fc93435771b1527e6

--- src/tests/ui_tests/conftest.py
+++ src/tests/ui_tests/conftest.py
@@ -1,5 +1,4 @@
 import os
-
 import allure
 import pytest
 from selenium import webdriver
@@ -7,12 +6,13 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.ie.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from utils.excel_util import save_test_result
+
 
 # 웹 실행
 @pytest.fixture(scope="module")
 def driver():
     chrome_options = Options()
-
     chrome_options.add_argument("--disable-dev-shm-usage") #메모리 부족 방지
     chrome_options.add_argument("--no-sandbox") #샌드박스 비활성화
     chrome_options.add_argument("--headless=new") #GUI없는 환경에서도 실행 가능
@@ -29,24 +29,31 @@ def driver():
     driver.implicitly_wait(10)
 
     yield driver   # 테스트 실행
-
-    driver.quit()  # 모든 테스트 완료 후 브라우저 종료s
+    driver.quit()  # 모든 테스트 완료 후 브라우저 종료
 
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_makereport(item, call):
+def pytest_runtest_makereport(item):
     outcome = yield
     report = outcome.get_result()
+    test_name = item.name
 
-    if report.when == "call" and report.failed:
-        driver = item.funcargs.get("driver")
-        if driver:
-            screenshots_dir = "failed_screenshots"
-            os.makedirs(screenshots_dir, exist_ok=True)
+    if report.when == "call":
+        if report.passed:
+            save_test_result(test_name, "PASS")
+        elif report.failed:
+            save_test_result(test_name,"FAIL", str(report.longrepr))
 
-        screenshot_path = os.path.join(screenshots_dir, f"{item.name}.png")
 
-        driver.save_screenshot(screenshot_path)
-        allure.attach.file(screenshot_path, name="Failure Screenshot", attachment_type=allure.attachment_type.PNG)\
+            driver = item.funcargs.get("driver", None)
+            if driver:
+                screenshots_dir = "failed_screenshots"
+                os.makedirs(screenshots_dir, exist_ok=True)
+
+            screenshot_path = os.path.join(screenshots_dir,f"{test_name}.png")
+            driver.save_screenshot(screenshot_path)
+
+            allure.attach.file(screenshot_path, name="Failure Screenshot", attachment_type=allure.attachment_type.PNG)
+
 
 
 
