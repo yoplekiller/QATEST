9e10c7ea2fc52dfeb37d937f68549f2583677113
yoplekiller <jmlim9244@gmail.com>
2025-03-27 06:59:36 +0900

    fix

--- src/tests/ui_tests/conftest.py
+++ src/tests/ui_tests/conftest.py
@@ -36,20 +36,7 @@ def driver():
     driver.quit()
 
 
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    outcome = yield
-    report = outcome.get_result()
 
-    if report.when == "call" and report.failed:
-        driver = item.funcargs.get("driver")
-        if driver:
-            screenshots_dir = "failed_screenshots"
-            os.makedirs(screenshots_dir, exist_ok=True)
-            screenshot_name = f"{item.name}_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
-            screenshot_path = os.path.join(screenshots_dir, screenshot_name)
-            driver.save_screenshot(screenshot_path)
-            allure.attach.file(screenshot_path, name="Failure Screenshot", attachment_type=allure.attachment_type.PNG)
 
 
 
