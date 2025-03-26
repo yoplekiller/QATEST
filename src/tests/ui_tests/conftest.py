dd41e958d57068616012c1bd2c1852c0134e0958
yoplekiller <jmlim9244@gmail.com>
2025-03-26 18:17:38 +0900

    fix

--- src/tests/ui_tests/conftest.py
+++ src/tests/ui_tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.ie.service import Service
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 # 웹 실행
