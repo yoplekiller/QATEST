a1f317e626491f382de936d9ef3e975fab0d6615
yoplekiller <yoplekiller@users.noreply.github.com>
2025-03-26 22:38:03 +0000

    deploy: 63af5bb37e0402e4e7c6e13dcd3d2664321fef48

--- src/tests/ui_tests/conftest.py
+++ src/tests/ui_tests/conftest.py
@@ -5,10 +5,9 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support import expected_conditions as EC
+
 
 @pytest.fixture(scope="module")
 def driver():
