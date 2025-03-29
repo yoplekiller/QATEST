5bf9ea6f97678bf82b1bef6f53848c3fddec4557
yoplekiller <yoplekiller@users.noreply.github.com>
2025-03-29 17:29:05 +0000

    deploy: 136b3d91b4cd8eb7fa9ea54c9685cbf98fd3f16f

--- src/tests/api_tests/test_moive_details.py
+++ src/tests/api_tests/test_moive_details.py
@@ -7,8 +7,9 @@ load_dotenv()
 API_KEY = os.getenv("TMDB_API_KEY")
 BASE_URL = "https://api.themoviedb.org/3"
 
-@allure.feature("API 테스트")
+@allure.feature("영화 목록")
 @allure.story("영화 상세 정보 API 테스트")
+@allure.title("영화 상세 정보 노출 되는지 확인")
 def test_get_movie_details():
     """특정 영화 상세 정보 API 테스트"""
     movie_id = 27205
