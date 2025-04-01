0ef2e8148c6115e092960b2bfe62a4e60e99d38e
yoplekiller <yoplekiller@users.noreply.github.com>
2025-04-01 08:37:20 +0000

    deploy: 50783f5caedd8e43fce7ab551ec902c4e73c7871

--- src/tests/api_tests/test_search_moive.py
+++ src/tests/api_tests/test_search_moive.py
@@ -9,7 +9,6 @@ BASE_URL = "https://api.themoviedb.org/3"
 
 @allure.feature("영화 목록")
 @allure.story("영화 검색 API 테스트")
-@allure.title("영화 검색 기능이 잘 동작 하는지 확인")
 def test_search_movie():
     """영화 검색 테스트"""
     params = {
