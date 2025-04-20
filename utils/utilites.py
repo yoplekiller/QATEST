f19cfa6e0125bec2518c369d639e47bedc69e76f
yoplekiller <yoplekiller@users.noreply.github.com>
2025-04-20 08:33:39 +0000

    deploy: 0e4736b87a92c14ba59162f92e9f1c5113bfce23

--- utils/utilites.py
+++ utils/utilites.py
@@ -1,33 +1,26 @@
 import os.path
 from datetime import datetime
-import allure
-
 
 #테스트 결과 캡쳐
-def capture_screenshot(driver, test_case_name, base_path = "screenshots", headless_mode=False, attach_to_allure=False):
+def capture_screenshot(driver, test_case_name, base_path = "screenshots", headless_mode=False):
     if headless_mode:
         return
 
     test_folder = os.path.join(base_path, test_case_name.replace(" ","_"))
     os.makedirs(test_folder, exist_ok=True)
-    print(f"📁 생성 시도 중: {test_folder}")
 
     #시간 가져오기
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_name = f"screenshots_{timestamp}.png"
-    screenshots_file_path = os.path.join(test_folder, file_name)
 
+    #파일명 생성
+    file_name = f"screenshots_{timestamp}.png"
+
+    #전체 경로
+    screenshots_file_path = os.path.join(test_folder, file_name)
 
     #스크린샷 저장
     driver.save_screenshot(screenshots_file_path)
     print(f"Screenshot saved at: {screenshots_file_path}")
-
-    # Allure 첨부 선택적으로
-    if attach_to_allure:
-        allure.attach.file(
-          screenshots_file_path,
-          name="Failure Screenshot",
-          attachment_type=allure.attachment_type.PNG)
 
     return screenshots_file_path
 
