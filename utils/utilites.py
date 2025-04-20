991a7c554efb4e1c965dcaebc91c154286afc510
yoplekiller <jmlim9244@gmail.com>
2025-04-20 21:04:21 +0900

    Fix

--- utils/utilites.py
+++ utils/utilites.py
@@ -1,33 +1,39 @@
 import os.path
-from datetime import datetime
+import datetime
+import re
 import allure
 
 
 #테스트 결과 캡쳐
-def capture_screenshot(driver, test_case_name, base_path = "screenshots", headless_mode=False, attach_to_allure=False):
+def capture_screenshot(driver, test_case_name, base_path ="screenshots", headless_mode=False, attach_to_allure=False):
     if headless_mode:
         return
 
-    test_folder = os.path.join(base_path, test_case_name.replace(" ","_"))
+    data_str = datetime.datetime.now().strftime("%Y-%m-%d")
+    base_path = os.path.join(base_path, data_str)
+
+    #테스트 케이스 이름 정리
+    safe_test_name = re.sub(r"[^\w\-_.]", "_", test_case_name)
+
+    #최종 폴더
+    test_folder = os.path.join(base_path, safe_test_name)
     os.makedirs(test_folder, exist_ok=True)
-    print(f"📁 생성 시도 중: {test_folder}")
 
-    #시간 가져오기
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_name = f"screenshots_{timestamp}.png"
-    screenshots_file_path = os.path.join(test_folder, file_name)
-
+    #파일명 생d S성
+    file_name = f"{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
+    file_path = os.path.join(test_folder, file_name)
 
     #스크린샷 저장
-    driver.save_screenshot(screenshots_file_path)
-    print(f"Screenshot saved at: {screenshots_file_path}")
+    driver.save_screenshot(file_path)
+    print(f"📸 스크린샷 저장 완료: {file_path}")
 
-    # Allure 첨부 선택적으로
     if attach_to_allure:
         allure.attach.file(
-          screenshots_file_path,
-          name="Failure Screenshot",
-          attachment_type=allure.attachment_type.PNG)
+            file_path,
+            name="Failure sreenshot",
+            attachment_type=allure.attachment_type.PNG
+        )
 
-    return screenshots_file_path
+
+
 
