eeaf67fb893ec9deceed07cf58e87392d09c14ab
임재민 <jmlim9244@gmail.com>
2025-04-25 19:16:12 +0900

    fix

--- src/tests/api_tests/test_moive_details.py
+++ src/tests/api_tests/test_moive_details.py
@@ -7,11 +7,15 @@ from utils.api_utils import send_get_request, attach_response
 @allure.feature("영화 목록")
 @allure.story("영화 상세 정보 API 테스트")
 @allure.title("유효한 movie_id에 대한 상세 정보 조회 테스트")
-@pytest.mark.parametrize("movie_id", [27025, 550, 603, 157336])
-def test_get_movie_details():
+@pytest.mark.parametrize("movie_id","expected_title",[
+        (27025, "The Godfather"),
+        (550, "Fight Club"),
+        (603, "The Matrix"),
+        (157336, "Interstellar")
+    ])
+def test_get_movie_details(movie_id, expected_title):
     """특정 영화 상세 정보 API 테스트"""
 
-    movie_id = 27205
     endpoint = f"/movie/{movie_id}"
     params = {
         "api_key": API_KEY
@@ -22,5 +26,5 @@ def test_get_movie_details():
 
     assert response.status_code == 200
     assert data["id"] == movie_id
-    assert data["title"] == "Inception"
+    assert data["title"] == expected_title
 
--- src/tests/ui_tests/test_ui_search.py
+++ src/tests/ui_tests/test_ui_search.py
@@ -2,7 +2,7 @@ import allure
 import pytest
 from selenium.webdriver import Keys
 import time
-from utils.excel_reader import read_search_terms_from_excel, file_path
+from utils.read_product_data import read_search_terms_from_excel, file_path
 from selenium.webdriver.common.by import By
 from utils.utilities import capture_screenshot
 
