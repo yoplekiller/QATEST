bce45272de6a3257a12b1816aa3d5bb22eba4729
yoplekiller <jmlim9244@gmail.com>
2025-04-27 23:20:18 +0900

    Fix

--- src/tests/api_tests/test_moive_details.py
+++ src/tests/api_tests/test_moive_details.py
@@ -7,7 +7,7 @@ from utils.api_utils import send_get_request, attach_response
 @allure.feature("영화 목록")
 @allure.story("영화 상세 정보 API 테스트")
 @allure.title("유효한 movie_id에 대한 상세 정보 조회 테스트")
-@pytest.mark.parametrize("movie_id","expected_title",[
+@pytest.mark.parametrize(["movie_id","expected_title"],[
         (27025, "The Godfather"),
         (550, "Fight Club"),
         (603, "The Matrix"),
@@ -20,6 +20,7 @@ def test_get_movie_details(movie_id, expected_title):
     params = {
         "api_key": API_KEY
     }
+
     response = send_get_request(endpoint, params)
     data= response.json()
     attach_response(response)
