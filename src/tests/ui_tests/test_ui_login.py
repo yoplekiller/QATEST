85fb0f1c3fd0b7b11c1573dd679b2ad16ee36f9f
임재민 <jmlim9244@gmail.com>
2025-05-08 19:17:41 +0900

    fix

--- src/tests/ui_tests/test_ui_login.py
+++ src/tests/ui_tests/test_ui_login.py
@@ -12,25 +12,26 @@ def test_login(driver):
     driver.maximize_window()
 
     try:
-        driver.get("https://www.kurly.com/main")
-        time.sleep(2)
         login_button = driver.find_element(By.XPATH, "//a[contains(text(),'로그인')]")
         login_button.click()
         time.sleep(2)
 
         #아이디 패스워드 입력
-        login_username_input = driver.find_element(By.NAME, "id")
-        login_password_input = driver.find_element(By.ID, "pw")
-        login_submit_button = driver.find_element(By.ID,"log.login")
-        time.sleep(2)
+        login_username_input = driver.find_element(By.XPATH, "//input[@placeholder='아이디를 입력해주세요']")
+        login_password_input = driver.find_element(By.XPATH, "//input[@placeholder='비밀번호를 입력해주세요']")
+        login_submit_button = driver.find_element(By.XPATH,"//button[@type='submit']")
 
         login_username_input.clear()
         login_password_input.clear()
 
         login_username_input.send_keys('boksoon8')
         login_password_input.send_keys('1232133123')
+        time.sleep(3)
         login_submit_button.click()
         time.sleep(3)
+
+        success_case = driver.find_element(By.XPATH, "//div[@class='popup-content css-15yaaju e1k5padi2']")
+        assert success_case.is_displayed(), f"❌ 로그인 실패"
 
     except Exception as e:
         capture_screenshot(driver, "로그인", "screenshots_login")
